06a5d4599476c6d35e02dd0a521d1d083bd06dc1
Arootsy <aronrootsaert07@gmail.com>
2025-07-10 10:23:43 +0200

    feat(config) adjust AFK timer settings for improved user experience

--- config/config.lua
+++ config/config.lua
@@ -1,8 +1,8 @@
 return {
     afkZone = 0.3,              -- Radius (in meters) around the player where movement resets AFK timer
-    afkMinutes = 10,            -- Total allowed AFK time in minutes
+    afkMinutes = 0.5,           -- Total allowed AFK time in minutes
 
-    preAfkPercent = 90,         -- Percentage of total AFK time to wait silently (no countdown shown)
+    preAfkPercent = 20,         -- Percentage of total AFK time to wait silently (no countdown shown)
                                 -- For example, 90 means 90% of afkMinutes will pass before countdown starts
 
     countdownPercent = 10,      -- Percentage of total AFK time when countdown is visible and ticking down
